dce80492d6e8d519719ee69fc6e53dc609b332f8
nancy-ni <nni@fi.uba.ar>
2026-09-14 16:00:49 -0300

    Se emprolija consumer tag

--- golang/internal/factory/utils.go
+++ golang/internal/factory/utils.go
@@ -5,8 +5,6 @@ import (
 	"math/rand/v2"
 )
 
-const MAX_CONSUMERTAG_NUMBER = 999999999
-
 func generateNewConsumerTag(name string) string {
-	return fmt.Sprintf("consumer-tag-%s-%d", name, rand.IntN(MAX_CONSUMERTAG_NUMBER))
+	return fmt.Sprintf("consumer-tag-%s-%d", name, rand.Uint64())
 }
